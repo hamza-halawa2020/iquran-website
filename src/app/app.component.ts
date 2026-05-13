a958362d7af4508a73207b8d8df3bcbba0ecbee7
hamza-halawa2020 <h.halawa2020@gmail.com>
2026-05-13 20:59:08 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,13 @@ import { ViewportScroller } from '@angular/common';
 import { RouterOutlet, Router, Event, NavigationEnd } from '@angular/router';
 import { NavbarComponent } from './common/navbar/navbar.component';
 import { WhatsappFloatComponent } from './common/whatsapp-float/whatsapp-float.component';
-import { CustomCursorComponent } from './shared/components/custom-cursor/custom-cursor.component';
 import { FooterComponent } from './common/footer/footer.component';
 import { BackToTopComponent } from './common/back-to-top/back-to-top.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, NavbarComponent, WhatsappFloatComponent, CustomCursorComponent, FooterComponent, BackToTopComponent],
+    imports: [RouterOutlet, NavbarComponent, WhatsappFloatComponent,  FooterComponent, BackToTopComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
