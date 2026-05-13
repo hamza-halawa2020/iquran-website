ca07645a5bd0d18972ab681cb1d85614b31aff48
hamza-halawa2020 <h.halawa2020@gmail.com>
2026-05-13 20:49:38 +0300

    update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    it(`should have the 'onlineislamicschool' title`, () => {
+    it(`should have the 'iquran' title`, () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
-        expect(app.title).toEqual('onlineislamicschool');
+        expect(app.title).toEqual('iquran');
     });
 
     it('should render title', () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('h1')?.textContent).toContain('Hello, onlineislamicschool');
+        expect(compiled.querySelector('h1')?.textContent).toContain('Hello, iquran');
     });
 });
