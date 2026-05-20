f29dd86f951f8aeb8a78053a8c84bd42aaa3b140
hamza-halawa2020 <h.halawa2020@gmail.com>
2026-05-20 23:59:30 +0300

    update

--- src/app/pages/courses-page/course-details/course-details.component.ts
+++ src/app/pages/courses-page/course-details/course-details.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CoursesService } from '../courses.service';
 import { TranslateModule } from '@ngx-translate/core';
+import { environment } from '../../../../environments/environment';
 
 @Component({
     selector: 'app-course-details',
@@ -13,6 +14,7 @@ import { TranslateModule } from '@ngx-translate/core';
 })
 export class CourseDetailsComponent implements OnInit {
     course: any;
+    processedDescription = '';
     isLoading: boolean = true;
 
     constructor(
@@ -34,11 +36,45 @@ export class CourseDetailsComponent implements OnInit {
         this.coursesService.getCourseDetails(id).subscribe({
             next: (response: any) => {
                 this.course = response.data;
+                this.processedDescription = this.normalizeCourseHtml(this.course?.description || '');
                 this.isLoading = false;
             },
             error: (error: any) => {
                 this.isLoading = false;
             }
         });
+    }
+
+    private normalizeCourseHtml(html: string): string {
+        if (!html || typeof window === 'undefined') {
+            return html || '';
+        }
+
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        const backendBase = (environment.imgUrl || '').replace(/\/$/, '');
+
+        doc.querySelectorAll('a').forEach((anchor) => {
+            const href = anchor.getAttribute('href') || '';
+            if (!href) {
+                return;
+            }
+
+            if (href.startsWith('/')) {
+                anchor.setAttribute('href', `${backendBase}${href}`);
+            }
+
+            anchor.setAttribute('target', '_blank');
+            anchor.setAttribute('rel', 'noopener noreferrer');
+        });
+
+        doc.querySelectorAll('img').forEach((img) => {
+            const src = img.getAttribute('src') || '';
+            if (src.startsWith('/')) {
+                img.setAttribute('src', `${backendBase}${src}`);
+            }
+        });
+
+        return doc.body.innerHTML;
     }
 }
